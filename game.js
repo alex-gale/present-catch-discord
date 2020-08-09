b3c7cdcdfd15ef3b89f9c9df88db179226524560
Alex Gale <alexgaleiii@gmail.com>
2020-08-09 14:12:42 +0100

    Add game

---
+++ game.js
@@ -0,0 +1,164 @@
+const Discord = require('discord.js')
+const GraphemeSplitter = require('grapheme-splitter')
+
+const splitter = new GraphemeSplitter()
+
+const GAME_WIDTH = 11
+const GAME_HEIGHT = 11
+const PIECES = {
+  b: '⬛',
+  s: '🎅🏻',
+  p: '🎁'
+}
+
+const _2dArray = (w, h, val) => {
+  var arr = []
+  for (let i = 0; i < h; i++) {
+    arr[i] = []
+    for (let j = 0; j < w; j++) {
+      arr[i][j] = val
+    }
+  }
+  return arr
+}
+
+// renders array to string of emojis
+const _render = (board) => {
+  let text = ''
+
+  for (const y of board) {
+    for (const x of y) {
+      text += PIECES[x]
+    }
+
+    text += '\n'
+  }
+
+  return text
+}
+
+// turns string of emojis into array
+const _derender = (text) => {
+  const gameBoard = _2dArray(GAME_WIDTH, GAME_HEIGHT, 'b')
+  const textSplit = text.split('\n').map(line => splitter.splitGraphemes(line))
+
+  textSplit.forEach((line, i) => {
+    line.forEach((char, j) => {
+      gameBoard[i][j] = Object.keys(PIECES).find(key => PIECES[key] === char)
+    })
+  })
+
+  return gameBoard
+}
+
+const initiate = async (bot, msg, channel) => {
+  channel.send(`${msg.author}`).then(message => message.delete())
+
+  const gameBoard = _2dArray(GAME_WIDTH, GAME_HEIGHT, 'b')
+  gameBoard[gameBoard.length - 1][5] = 's'
+
+  const x = Math.floor(Math.random() * 11)
+  gameBoard[0][x] = 'p'
+
+  channel.send(new Discord.MessageEmbed()
+    .setTitle('The Elves are all drunk again and are throwing all the presents out of a helicopter. Help Santa to catch them all and save Christmas!')
+    .setDescription(_render(gameBoard))
+    .setColor('#ffffff')
+    .addField('Score', '0 / 10', true)
+    .addField('Frame', '0', true)
+  ).then(message => {
+    message.react('⬅️')
+    message.react('➡️')
+    message.react('⬇️')
+    message.react('❌')
+  })
+}
+
+const action = async (bot, reaction, user) => {
+  const gameMessage = reaction.message
+  const gameChannel = reaction.message.channel
+  const action = reaction.emoji.name
+  let score = parseInt(gameMessage.embeds[0].fields[0].value)
+  let frame = parseInt(gameMessage.embeds[0].fields[1].value)
+  frame += 1
+
+  if (action === '❌') {
+    gameChannel.delete()
+  } else {
+    // calculate game board
+    const gameString = reaction.message.embeds[0].description
+    const gameBoard = _derender(gameString)
+    const playerX = gameBoard[gameBoard.length - 1].findIndex(n => n === 's')
+
+    // handle player movement
+    if (action === '⬅️') {
+      if (playerX > 0) {
+        if (gameBoard[gameBoard.length - 1][playerX - 1] === 'p') {
+          score += 1
+        }
+
+        gameBoard[gameBoard.length - 1][playerX] = 'b'
+        gameBoard[gameBoard.length - 1][playerX - 1] = 's'
+      }
+    } else if (action === '➡️') {
+      if (playerX < gameBoard[0].length - 1) {
+        if (gameBoard[gameBoard.length - 1][playerX + 1] === 'p') {
+          score += 1
+        }
+
+        gameBoard[gameBoard.length - 1][playerX] = 'b'
+        gameBoard[gameBoard.length - 1][playerX + 1] = 's'
+      }
+    }
+
+    // present gravity
+    let presentCount = 0
+    gameBoard.forEach((line, i) => {
+      if (line.includes('p')) {
+        const presentX = gameBoard[i].findIndex(n => n === 'p')
+        gameBoard[i][presentX] = 'b'
+
+        if (i + 1 < GAME_HEIGHT) {
+          if (gameBoard[i + 1][presentX] === 's') {
+            // if player is at next position
+            score += 1
+          } else {
+            gameBoard[i + 1][presentX] = 'pe'
+          }
+        }
+      } else if (line.includes('pe')) {
+        const presentX = gameBoard[i].findIndex(n => n === 'pe')
+        gameBoard[i][presentX] = 'p'
+        presentCount += 1
+      }
+    })
+
+    // present spawning
+    if (frame % 5 === 0 && frame <= 45) {
+      const newPresentX = Math.floor(Math.random() * 11)
+      gameBoard[0][newPresentX] = 'p'
+      presentCount += 1
+    }
+
+    if (presentCount > 0) {
+      gameMessage.edit(new Discord.MessageEmbed()
+        .setDescription(_render(gameBoard))
+        .setColor('#ffffff')
+        .addField('Score', `${score} / 10`, true)
+        .addField('Frame', frame, true)
+      ).then(() => {
+        // delete reaction
+        reaction.users.remove(user.id)
+      })
+    } else {
+      gameMessage.delete()
+      gameChannel.send(`Game Over! You caught ${score} / 10 presents. Closing channel in 3 seconds...`)
+
+      setTimeout(() => {
+        gameChannel.delete()
+      }, 3000)
+    }
+  }
+}
+
+module.exports = { initiate, action }
---
+++ index.js
@@ -0,0 +1,94 @@
+const Discord = require('discord.js')
+const shortid = require('shortid')
+const bot = new Discord.Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION'] })
+
+const game = require('./game')
+
+shortid.characters('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ._')
+
+const prefix = '!'
+
+bot.on('ready', () => {
+  bot.user.setActivity(`Present Catch - ${prefix}present`)
+  console.log('Present Catch Bot Started')
+
+  // clear out old channels
+  const managedChannels = bot.channels.cache.array().filter(c => c.name.startsWith('🎁'))
+  managedChannels.forEach(channel => {
+    channel.messages.fetch({ limit: 1 })
+      .then(messages => {
+        if (messages.last().content) {
+          channel.delete()
+        }
+      })
+  })
+})
+
+bot.on('messageReactionAdd', async (reaction, user) => {
+  // add reaction to cache if partial
+  if (reaction.partial) {
+    try {
+      await reaction.fetch()
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  const VALID_REACTIONS = ['⬅️', '➡️', '⬇️', '❌']
+  // delete if not valid reaction
+  if (!VALID_REACTIONS.includes(reaction.emoji.name)) return reaction.remove()
+  // check channel and reaction count
+  if (reaction.count < 2 || !reaction.message.channel.name.startsWith('🎁')) return
+
+  game.action(bot, reaction, user)
+})
+
+bot.on('message', msg => {
+  if (msg.author.bot) return
+  if (msg.channel.name.startsWith('🎁') || msg.channel.name !== 'start-game') return msg.delete()
+  if (msg.content !== '!present') return
+
+  var guild = msg.guild
+
+  const managedChannels = guild.channels.cache.array().filter(c => c.name.startsWith('🎁'))
+  const channelName = `🎁-${msg.author.tag.toLowerCase().replace('#', '')}`
+
+  if (managedChannels.map(c => c.name).includes(channelName)) {
+    return msg.channel.send(new Discord.MessageEmbed()
+      .setDescription(`${msg.author} You are already playing Present Catch!`)
+      .setColor('#bd230e')
+    ).then(warning => {
+      msg.delete()
+      setTimeout(() => {
+        warning.delete()
+      }, 3000)
+    })
+  }
+
+  guild.channels.create(channelName, {
+    permissionOverwrites: [
+      {
+        id: guild.id,
+        deny: ['VIEW_CHANNEL']
+      },
+      {
+        id: bot.user.id,
+        allow: ['VIEW_CHANNEL', 'SEND_MESSAGES']
+      },
+      {
+        id: msg.author.id,
+        allow: ['VIEW_CHANNEL'],
+        deny: ['SEND_MESSAGES']
+      }
+    ]
+  }).then((channel) => {
+    const category = guild.channels.cache.find(c => c.name === 'Present Catch' && c.type === 'category')
+    if (!category) throw new Error('Present Catch category channel does not exist')
+    channel.setParent(category.id)
+
+    game.initiate(bot, msg, channel)
+    msg.delete()
+  }).catch(console.error)
+})
+
+bot.login('Mzk5MTkxNzUwMjM5MDU5OTc5.WlDOSQ._Qr_n3G9pU-1i7id4llBSnrbTHc')
